4d89f321b2cf1158472a7b8d522c4d130e82f2fb
d3vr <d3vr@mitidji.com>
2018-05-18 01:16:15 +0100

    - Fixed: not working for all images. Also, it now works for related images as well. - Added version indicator to the corner of the page, user can know if there's an update available.

--- view_image.js
+++ view_image.js
@@ -1,8 +1,11 @@
+var version = "1.3";
+
 if (typeof window.isElementVisible === 'undefined') {
     window.isElementVisible = isElementVisiblePolyfill;
 }
 
 // Handy function from StackOverflow: https://stackoverflow.com/a/15203639/2733526
+// Used to detect if an element is visible in the viewport
 function isElementVisiblePolyfill(el) {
     var rect     = el.getBoundingClientRect(),
         vWidth   = window.innerWidth || doc.documentElement.clientWidth,
@@ -23,10 +26,36 @@ function isElementVisiblePolyfill(el) {
     );
 }
 
+// Another handy function from StackOverflow: https://stackoverflow.com/a/35385518/2733526
+// Used to add new HTML elements from HTML code
+function htmlToElement(html) {
+    var template = document.createElement('template');
+    html = html.trim(); // Never return a text node of whitespace as the result
+    template.innerHTML = html;
+    return template.content.firstChild;
+}
+
+if(!document.querySelector("#viewimage_version")){
+    document.body.appendChild(htmlToElement('<a id="viewimage_version" href="https://d3vr.github.io/viewimage/" style="position:fixed; z-index:999; top: 0; right:0;"><img src="https://d3vr.me/viewimage/version.php?v='+version+'" height="30"></a>'))
+}
+
 var imgNames = document.querySelectorAll("#irc_cc>div[data-item-id]");
 imgNames.forEach(function(img){
     if(isElementVisible(img)){
-        var img_json = JSON.parse(document.querySelector("img[name='"+img.dataset.itemId+"']").parentNode.nextSibling.childNodes[0].nodeValue);
-        window.open(img_json.ou);
+        var itemId = img.dataset.itemId;
+        var reference_element = document.getElementById(itemId);
+        // Find the text node containing the itemId (which has the JSON we need)
+        var img_json = document.evaluate('//*[text()[contains(.,"'+itemId+'")]]' ,document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue;
+
+        // This is a related image
+        if(!img_json){
+            img_json = document.querySelectorAll("[data-item-id='"+itemId+"']")[1].childNodes[1].childNodes[0].nodeValue;
+        // This is a main image
+        }else{
+            img_json = img_json.childNodes[0].nodeValue;
+        }
+
+        // Open the image source in a new tab
+        window.open(JSON.parse(img_json).ou);
     }
 });
